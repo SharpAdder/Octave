c5bae4506bfebdf7ca6399cbd47008d6356145d9
Adina Adam <49348559+SharpAdder@users.noreply.github.com>
2022-05-15 20:09:09 +0300

    Create access_vector_elem.m

---
+++ access_vector_elem.m
@@ -0,0 +1,17 @@
+% line vector
+W = [3 2 1]
+
+% column vector
+X = [6;7;8]
+
+% matrix 2 lines, 3 columns
+A = [3 7 9 ; 5 8 1]
+
+% accessing first element from W vector -> outputs 3
+W(1)
+ 
+% accessing second element from vector X -> outputs 7
+X(2)
+  
+% accessing element on: line2, column2 from matrix A -> outputs 8
+A(2,2)
