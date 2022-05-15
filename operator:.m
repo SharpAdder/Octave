d0ae7ef4510dc52237919b6c35a0c945a4fba75c
Adina Adam <49348559+SharpAdder@users.noreply.github.com>
2022-05-15 19:53:18 +0300

    Create operator:.m

---
+++ operator:.m
@@ -0,0 +1,23 @@
+>> %display all no in the interval 2,8
+>> Y=2:8
+Y =
+
+   2   3   4   5   6   7   8
+
+>> %stops at 7.5; 7.5<8
+>> y=2.5:8
+y =
+
+ Columns 1 through 5:
+
+    2.5000    3.5000    4.5000    5.5000    6.5000
+
+ Column 6:
+
+    7.5000
+
+>> % 10 to 1; -1 decremeting
+>> z=10:-1:1
+z =
+
+   10    9    8    7    6    5    4    3    2    1
